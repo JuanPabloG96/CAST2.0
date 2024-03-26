a3746523342fc982a9e4c831a60d37c0926f2573
Juan Pablo Gama <juanpablo.gama.olivares@gmail.com>
2024-03-26 11:16:13 -0600

    update: login.php

--- frontend/login.php
+++ frontend/login.php
@@ -4,19 +4,24 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <title>inicio de sesión</title>
+  <title>inicio de sesión CAST</title>
 </head>
 
 <body>
   <main class="main">
     <form action="/backend/routes/login.php" method="post" class="form">
+      <div>
+        <img src="public/images/uacj_logo.png" alt="uacj logo" width="340px">
+      </div>
+      <h2>Iniciar sesión</h2>
+      <small>Ingrese su correo y contraseña institucional</small>
       <div class="input_container">
         <label for="email">correo</label>
-        <input type="email" name="email" id="email" placeholder="ej. correo@ejemplo.com">
+        <input type="email" name="email" id="email" placeholder="Ingrese su correo">
       </div>
       <div class="input_container">
         <label for="password">contraseña</label>
-        <input type="password" name="password" id="password" placeholder="ingrese contraseña">
+        <input type="password" name="password" id="password" placeholder="Ingrese su contraseña">
       </div>
     </form>
   </main>

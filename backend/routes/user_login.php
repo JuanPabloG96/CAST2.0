3d397c2129a9482f4695328c2f1320fcbc8730bc
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-03-27 02:47:19 -0600

    fix: temp verification data user_login.php

--- backend/routes/user_login.php
+++ backend/routes/user_login.php
@@ -1,17 +1,28 @@
 <?php
 include("/laragon/www/proyecto_cast/backend/db.php");
-
-$user = "Test user";
-$email = $_POST["email"];
-$password = $_POST["password"];
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
 $connection = db_connection();
 
-$sql = "INSERT INTO tbl_test (user, email, password) VALUES ('$user', '$email', '$password')";
-if (mysqli_query($connection, $sql)) { 
-    echo " Datos guardados correctamente";
-} else {
-    echo "Error al guardar los datos: " . mysqli_error($connection);
+$user = $email = $password = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user = test_input('Test user');
+    $email = test_input($_POST["email"]);
+    $password = test_input($_POST["password"]);
+
+    $sql = "INSERT INTO tbl_test (user, email, password) VALUES ('$user', '$email', '$password')";
+    if (mysqli_query($connection, $sql)) {
+        echo " Datos guardados correctamente";
+    } else {
+        echo "Error al guardar los datos: " . mysqli_error($connection);
+    }
 }
 
 $connection->close();
